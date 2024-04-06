b8204312c575e6340ae8371558d109d07f4d735b
venomcodes0943 <dhcodes0943@gmail.com>
2024-04-06 18:16:07 +0500

    Login With Auth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Str;
 
 class UserController extends Controller
